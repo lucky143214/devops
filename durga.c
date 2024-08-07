8d2aa7e0bc5389a94db3d684f5fc8e8d5b736a8b
durga <darlingdurga493@gmail.com>
2024-08-07 20:12:11 +0530

    first commit

---
+++ durga.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	printf("enter number");
+	scanf("%d",&a);
+	printf("enter number");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("%d",sum);
+	return 0;
+}
